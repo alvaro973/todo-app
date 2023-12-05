12154f2b86fc628522f851fc6afd7a835f75c768
alvaro <alvaroaugusto3a2@gmail.com>
2023-12-05 10:30:37 -0300

    filtrando as tarefas ativas

--- index.js
+++ index.js
@@ -34,6 +34,25 @@ app.post('/completar', (requisicao, resposta) => {
     })
 })
 
+app.post('/descompletar', (requisicao, resposta) => {
+    const id = requisicao.body.id
+
+    console.log(id)
+
+    const sql = `
+        UPDATE tarefas
+        SET completa = '0'
+        WHERE id = ${id}
+    `
+
+    conexao.query(sql, (erro) => {
+        if (erro) {
+            return console.log(erro)
+        }
+        resposta.redirect('/')
+    })
+})
+
 app.post('/criar', (requisicao, resposta) =>{
     const decricao = requisicao.body.decricao
     const completa = 0
@@ -68,7 +87,13 @@ app.get('/',(requisicao, resposta) => {
         }
        })
 
-       resposta.render('home', {tarefas})
+       const tarefasAtivas = tarefas.filter((tarefa) => {
+        return tarefa.completa === false && tarefa
+       })
+
+       const quantidadeTarefasAtivas = tarefasAtivas.length
+
+       resposta.render('home', {tarefas, quantidadeTarefasAtivas})
     })
 })
 
